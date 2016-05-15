e447f297cfdbc5a4cf9cbf850659dc2617efbaae
Michael J. Mitchell <michael@mitchtech.net>
2016-05-14 17:27:14 -0700

    bootstrap example django-mezzanine project

---
+++ django-mezzanine/example/example/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for example project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("example"))
+
+application = get_wsgi_application()
